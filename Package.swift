bff4117f8a6d1bcea4e94004b9ae4141d9a7cb16
Brian Strobach <brian@appsaurus.io>
2018-07-19 00:21:18 -0500

    Updated to Fluent 3.0

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
     ],
     dependencies: [
 		.package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
-		.package(url: "https://github.com/vapor/fluent.git", .upToNextMajor(from:"3.0.0-rc.3.0.1")),
-		.package(url: "https://github.com/vapor/fluent-sqlite.git", .upToNextMajor(from:"3.0.0-rc.3.0.1")),
+		.package(url: "https://github.com/vapor/fluent.git", .upToNextMajor(from:"3.0.0")),
+		.package(url: "https://github.com/vapor/fluent-sqlite.git", .upToNextMajor(from:"3.0.0")),
 		.package(url: "https://github.com/Appsaurus/FluentSeeder", .upToNextMajor(from: "0.1.0"))
     ],
     targets: [
