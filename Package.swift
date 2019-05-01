930a40c1a17eb9ad82acc7080cc9f07ccceea6bb
Brian Strobach <brian@appsaurus.io>
2019-05-01 16:59:41 -0500

    Swift 5 tested on Linux and macOS

--- Package.swift
+++ Package.swift
@@ -13,9 +13,9 @@ let package = Package(
     ],
     dependencies: [
 		.package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
-		.package(url: "https://github.com/vapor/fluent.git", .upToNextMajor(from:"3.0.0")),
-		.package(url: "https://github.com/vapor/fluent-sqlite.git", .upToNextMajor(from:"3.0.0")),
-		.package(url: "https://github.com/Appsaurus/FluentSeeder", .upToNextMajor(from: "0.1.0"))
+		.package(url: "https://github.com/vapor/fluent.git", from:"3.0.0"),
+		.package(url: "https://github.com/vapor/fluent-sqlite.git", from:"3.0.0"),
+		.package(url: "https://github.com/Appsaurus/FluentSeeder", from: "0.1.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
